7a4122b725fe8238a854f89a241e4da079a8673a
Гонтаренко Дмитрий <dmitriy.gontarenko@accenture.com>
2019-10-31 14:43:44 +0300

    Удалил некоторые файлы и комментарии, обновил Wiki

--- src/main/java/com/accenture/usefultricks/logback/CustomLogback.java
+++ src/main/java/com/accenture/usefultricks/logback/CustomLogback.java
@@ -1,31 +1,12 @@
 package com.accenture.usefultricks.logback;
 
-import ch.qos.logback.classic.pattern.ClassicConverter;
-import ch.qos.logback.classic.spi.ILoggingEvent;
 import com.accenture.usefultricks.Application;
-import com.accenture.usefultricks.exception.CustomException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CustomLogback extends ClassicConverter {
+public class CustomLogback {
 
     private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
-
-    private long start = System.nanoTime();
-
-    /**
-     * Метод для тестирования конфигурации Evaluators в logback
-     */
-    public void evaluators() {
-        LOGGER.info("Hello there");
-        for (int i = 0; i < 3; i++) {
-            if (i == 1) {
-                LOGGER.info("logging statement " + i, new CustomException("do not display"));
-            } else {
-                LOGGER.info("logging statement " + i, new Exception("display"));
-            }
-        }
-    }
 
     public void showAllLevels() {
         LOGGER.info("Custom info 1");
@@ -35,13 +16,4 @@ public class CustomLogback extends ClassicConverter {
         LOGGER.warn("Custom warn 5");
     }
 
-    /**
-     * Данными метод возвращает количество наносекунд, прошедших с
-     * момента его создания.
-     */
-    @Override
-    public String convert(ILoggingEvent iLoggingEvent) {
-        long nowInNanos = System.nanoTime();
-        return Long.toString(nowInNanos - start);
-    }
 }
